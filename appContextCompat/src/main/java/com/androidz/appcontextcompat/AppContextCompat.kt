90c97ce28e77a9861730d786da1301d29a407fe5
iflove <1772885836@qq.com>
2020-10-11 23:21:40 +0800

    [ *]demo

---
+++ appContextCompat/src/main/java/com/androidz/appcontextcompat/AppContextCompat.kt
@@ -0,0 +1,13 @@
+package com.androidz.appcontextcompat
+
+import android.content.Context
+
+/**
+ *
+ * Created by rentianlong on 2020/10/11
+ */
+object AppContextCompat {
+
+    lateinit var context: Context
+
+}
